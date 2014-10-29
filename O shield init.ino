df7ee1848bb49a02f4b7eb178408c97afabfad53
Marsfan <Lockheedmaniac@gmail.com>
2014-10-29 10:28:29 -0500

    Changed for loops to be array based.
    Sorry about last commit, description was bad.

--- O shield init.ino
+++ O shield init.ino
@@ -2,7 +2,7 @@ int inputPins = [2,3,4,5,6,7];
 int outputPins = [8,9,10,11,12,13];
 
 void setup(){
-	for(int i =0; i < inputPins.length; i++){
+	for(int i = 0; i < inputPins.length; i++){
 		pinMode(inputPins[i], INPUT);
 	}
 	for(int i = 0; i < outputPins.length; i++){
