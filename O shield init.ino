8dfaf9c33a981d320c7335adbc9ffe15db83b516
Marsfan <Lockheedmaniac@gmail.com>
2014-10-29 10:25:42 -0500

    Added

--- O shield init.ino
+++ O shield init.ino
@@ -1,9 +1,12 @@
+int inputPins = [2,3,4,5,6,7];
+int outputPins = [8,9,10,11,12,13];
+
 void setup(){
-	for(int i = 2; i < 8; i++){
-		pinMode(i, INPUT);
+	for(int i =0; i < inputPins.length; i++){
+		pinMode(inputPins[i], INPUT);
 	}
-	for(int i = 8; i < 14; i++){
-		pinMode(i, OUTPUT);
+	for(int i = 0; i < outputPins.length; i++){
+		pinMode(outputPins[i], OUTPUT);
 	}
 	Serial.begin(9600);
 }
